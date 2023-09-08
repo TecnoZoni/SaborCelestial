f365391b85952bae4c649b8735fdedea1100cd71
Agustin Minzoni <agustinminzoni@gmail.com>
2023-09-08 18:14:39 -0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              { src: 'images/slides/slide1.jpg' },
+              { src: 'images/slides/slide2.jpg' },
+              { src: 'images/slides/slide3.jpg' }
+          ],
+          timer: false,
+      });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
